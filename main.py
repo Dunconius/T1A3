742f8e7ae8515ea3d651cb69a8d1d36c80d86158
Dunconius <duncanbean84@gmail.com>
2023-12-15 09:05:22 +1100

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import random
+
+
+cash = random.randint(1, 100)
+
+print("Welcome to Food ‘n’ Things. We sell all the food, and most of the things.")
+print(f"You have ${cash} in your wallet.")
+
+
+

bc58c1e1db591ab2ee7e99b63e216752938b95ab
Dunconius <duncanbean84@gmail.com>
2023-12-17 08:33:26 +1100

    implemented add_items, remove_items, view_basket

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import random
-# this is the counter function import
-from collections import Counter
-# from shopping_functions import *
 
+# starting item definitions -----------------------------------------------
 items_for_sale = {
     "milk": 4,
     "eggs": 8,
@@ -10,47 +8,79 @@ items_for_sale = {
     "meat":17
 }
 
-user_input_dict = {}
-total_cost = 0
+items_with_quantities = {}
+
 cash = random.randint(87, 129)
 
-def add_item():
-    # user_input_dict = {}
+# function that gets user to input list items -----------------------------------------------
+def add_item(items_with_quantities):
 
     while True:
-        item = input("Enter name of item (type 'done' to finish): ")
+        item = input("Enter an item to add (or 'done' to finish): ")
+        
         if item.lower() == 'done':
             break
-
-        try:
-            qty = int(input("Enter the quantity: "))
-        except ValueError:
-            print("Invalid input. Please enter a valid number.")
-            continue
-    
-        user_input_dict[item] = qty
-    return user_input_dict
-    
         
-def remove_item():
-    pass
+        try:
+            quantity = int(input(f"Enter the quantity for {item}: "))
+        except ValueError:
+            print("Invalid quantity. Please enter a valid number.")
+            continue
 
-def view_basket():
-    print("\nUser Shopping List:")
-    for item, quantity, price in user_input_dict:
-        print(f"Item: {item}, Quantity: {quantity}, Price per item: ${price}")
+        # Check if the item already exists in the dictionary
+        if item in items_with_quantities:
+            # If it does, update the quantity
+            items_with_quantities[item] += quantity
+        else:
+            # If it doesn't, add a new entry
+            items_with_quantities[item] = quantity
 
-    print(f"\nTotal Cost of Selected Items: ${total_cost}")
+    return list(items_with_quantities.items())
 
-def checkout():
-    pass
 
-def test_function():
-    print("test function!!!!")
+# function that gets user to remove list items -----------------------------------------------
+def remove_item(items_with_quantities):
+    print("Current items with quantities:", items_with_quantities)
 
-print("Welcome to Food ‘n’ Things. We sell all the food, and most of the things.")
-print(f"You have ${cash} in your wallet.")
-print("What would you like to buy today?")
+    while True:
+        item_to_remove = input("Enter an item to remove (or 'done' to finish): ")
+
+        if item_to_remove.lower() == 'done':
+            break
+
+        # Check if the item is in the dictionary
+        if item_to_remove in items_with_quantities:
+            while True:
+                try:
+                    quantity_to_remove = int(input(f"Enter the quantity to remove for {item_to_remove}: "))
+                    break
+                except ValueError:
+                    print("Invalid input. Please enter a valid number.")
+
+            # Check if the entered quantity is less than or equal to the existing quantity
+            if quantity_to_remove <= items_with_quantities[item_to_remove]:
+                items_with_quantities[item_to_remove] -= quantity_to_remove
+                print(f"Removed {quantity_to_remove} {item_to_remove}(s).")
+                if items_with_quantities[item_to_remove] == 0:
+                    del items_with_quantities[item_to_remove]
+            else:
+                print(f"Cannot remove more than {items_with_quantities[item_to_remove]} {item_to_remove}(s).")
+        else:
+            print(f"{item_to_remove} is not in the list. Please enter a valid item.")
+
+    return items_with_quantities
+
+
+def view_basket(items_with_quantities):
+    print("Your basket contains the following:")
+    for item, quantity in items_with_quantities.items():
+        print(f"{quantity} {item}.")
+    
+
+# user menu -----------------------------------------------
+# print("Welcome to Food ‘n’ Things. We sell all the food, and most of the things.")
+# print(f"You have ${cash} in your wallet.")
+# print("What would you like to buy today?")
 
 def create_menu():
     print("\nEnter 'add' to add items to basket")
@@ -67,11 +97,11 @@ while user_choice != "exit":
     user_choice = create_menu()
     user_choice = input("Enter your selection: ")
     if (user_choice == "add"):
-        add_item()
+        add_item(items_with_quantities)
     elif (user_choice == "remove"):
-        remove_item()
+        remove_item(items_with_quantities)
     elif (user_choice == "view"):
-        view_basket()
+        view_basket(items_with_quantities)
     elif (user_choice == "checkout"):
         checkout()
     elif (user_choice == "exit"):
